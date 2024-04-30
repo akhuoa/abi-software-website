9d45de81e21a27d0db4b232a6ace4b5d04f83990
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2024-04-30 14:16:39 +1200

    Move data to json file

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,64 +1,10 @@
 <script setup lang="ts">
 import { computed, ref } from 'vue'
+import data from '../data.json'
 
 defineProps<{ title: string }>()
 
-const repos = ref([
-  {
-    name: 'mapintegratedvuer',
-    url: 'https://github.com/ABI-Software/mapintegratedvuer',
-    api: 'https://abi-software.github.io/mapintegratedvuer/',
-    demo: ''
-  },
-  {
-    name: 'map-side-bar',
-    url: 'https://github.com/ABI-Software/map-sidebar',
-    api: 'https://abi-software.github.io/map-sidebar/',
-    demo: ''
-  },
-  {
-    name: 'FlatmapVuer',
-    url: 'https://github.com/ABI-Software/flatmapvuer',
-    api: 'https://abi-software.github.io/flatmapvuer/',
-    demo: ''
-  },
-  {
-    name: 'Scaffoldvuer',
-    url: 'https://github.com/ABI-Software/scaffoldvuer',
-    api: 'https://abi-software.github.io/scaffoldvuer/',
-    demo: ''
-  },
-  {
-    name: 'SimulationVuer',
-    url: 'https://github.com/ABI-Software/simulationvuer',
-    api: 'https://abi-software.github.io/simulationvuer/',
-    demo: ''
-  },
-  {
-    name: 'ABI-Software Image Dialog',
-    url: 'https://github.com/ABI-Software/image-dialog',
-    api: '',
-    demo: ''
-  },
-  {
-    name: 'plotvuer',
-    url: 'https://github.com/ABI-Software/plotvuer',
-    api: 'https://abi-software.github.io/plotvuer/',
-    demo: ''
-  },
-  {
-    name: 'ABI-Software Gallery',
-    url: 'https://github.com/ABI-Software/mapcore-gallery',
-    api: 'https://abi-software.github.io/mapcore-gallery/',
-    demo: ''
-  },
-  {
-    name: 'svg-sprite',
-    url: 'https://github.com/ABI-Software/svg-sprite',
-    api: 'https://abi-software.github.io/svg-sprite/',
-    demo: ''
-  }
-])
+const repos = ref(data)
 
 let search = ref('')
 
